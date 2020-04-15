623c5831990ccfdaa62f2b2b96d9af07b7cec71b
Carlos Sebastián Biscussi <cbiscussi@cooperativaobrera.coop>
2020-04-15 13:49:35 -0300

    trying to make github pages work

---
+++ docs/precache-manifest.ca0d5263b29253e527b07dc951695dcd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f6e5ecb928246b7126d71dcac6941f9",
+    "url": "/docs/index.html"
+  },
+  {
+    "revision": "3b0a4fb5107642a6fd9a",
+    "url": "/docs/static/css/main.b58cf5b2.chunk.css"
+  },
+  {
+    "revision": "fe234633bbf9152a3cb1",
+    "url": "/docs/static/js/2.5b3b68f4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/docs/static/js/2.5b3b68f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b0a4fb5107642a6fd9a",
+    "url": "/docs/static/js/main.ee84989f.chunk.js"
+  },
+  {
+    "revision": "32b4e995270750a19b1e",
+    "url": "/docs/static/js/runtime-main.3245f3ed.js"
+  }
+]);
