49f33ad4716c9e1f94829bc69f5ff8b50fc05b56
Carlos Sebastián Biscussi <cbiscussi@cooperativaobrera.coop>
2020-04-15 14:09:15 -0300

    changed homepage to github pages

---
+++ docs/precache-manifest.bca1ed510440a18a9a11620b93fb60ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d4c11d70118daf7580581d8b6840553",
+    "url": "/CV/index.html"
+  },
+  {
+    "revision": "6729098fc01aa225cab3",
+    "url": "/CV/static/css/main.b58cf5b2.chunk.css"
+  },
+  {
+    "revision": "fe234633bbf9152a3cb1",
+    "url": "/CV/static/js/2.5b3b68f4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/CV/static/js/2.5b3b68f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6729098fc01aa225cab3",
+    "url": "/CV/static/js/main.9b1f0d9f.chunk.js"
+  },
+  {
+    "revision": "d1b04f0e9ead92720269",
+    "url": "/CV/static/js/runtime-main.3f4eff89.js"
+  }
+]);
